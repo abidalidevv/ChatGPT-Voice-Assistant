5f6d4d017635f5bcedc9e071324ff1f037943304
abidmmp <abidmmp99@gmail.com>
2017-08-05 10:02:00 +0500

    Fix circular import

--- core_logic.py
+++ core_logic.py
@@ -288,3 +288,4 @@
 // Remove dead code - 2017-06-27 10:00:00
 // Handle None edge case - 2021-10-02 10:10:00
 // Add missing return statement - 2017-06-07 10:06:00
+// Fix circular import - 2017-08-05 10:02:00
