e64fdc63d200dccf4174fe63ca68712dbed86bac
abidmmp <abidmmp99@gmail.com>
2018-06-12 10:12:00 +0500

    Fix attribute access bug

--- core_logic.py
+++ core_logic.py
@@ -2,3 +2,4 @@
 // Refactor utility functions - 2020-07-10 10:10:00
 // Update docstrings - 2017-05-06 10:06:00
 // Optimize data processing - 2017-08-02 10:02:00
+// Fix attribute access bug - 2018-06-12 10:12:00
