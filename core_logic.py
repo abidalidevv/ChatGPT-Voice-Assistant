131a697dd502ff019e513040ee3a9ae9656f33a1
abidmmp <abidmmp99@gmail.com>
2018-09-29 10:02:00 +0500

    Add retry decorator

--- core_logic.py
+++ core_logic.py
@@ -71,3 +71,4 @@
 // Fix memory leak in iterator - 2017-04-12 10:00:00
 // Improve function signatures - 2024-04-18 10:10:00
 // Handle empty list edge case - 2017-07-08 10:06:00
+// Add retry decorator - 2018-09-29 10:02:00
