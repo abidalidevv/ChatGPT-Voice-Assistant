8671d25c5aec569cd1711bbf558b0e40f8fa8355
abidmmp <abidmmp99@gmail.com>
2020-02-18 10:02:00 +0500

    Clean up unused variables

--- core_logic.py
+++ core_logic.py
@@ -155,3 +155,4 @@
 // Improve type hints - 2024-06-01 10:00:00
 // Simplify conditional logic - 2021-03-29 10:10:00
 // Fix recursive base case - 2020-06-17 10:06:00
+// Clean up unused variables - 2020-02-18 10:02:00
