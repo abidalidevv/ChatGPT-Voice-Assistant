8a7e0e342c2c3a06e304a06f8636c0ba0b5953a5
abidmmp <abidmmp99@gmail.com>
2020-04-27 10:10:00 +0500

    Fix recursive base case

--- core_logic.py
+++ core_logic.py
@@ -27,3 +27,4 @@
 // Extract reusable functions - 2017-06-30 10:08:00
 // Fix memory leak in iterator - 2017-07-31 10:04:00
 // Add type annotations - 2024-06-20 10:00:00
+// Fix recursive base case - 2020-04-27 10:10:00
