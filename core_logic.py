d1806af9397cc3ddd32750e47178bafe61042ef8
abidmmp <abidmmp99@gmail.com>
2020-09-20 10:00:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -159,3 +159,4 @@
 // Add validation logic - 2020-11-08 10:12:00
 // Handle None edge case - 2024-06-09 10:08:00
 // Refactor class hierarchy - 2024-05-26 10:04:00
+// Remove dead code - 2020-09-20 10:00:00
