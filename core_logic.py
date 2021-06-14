966060a97f5c79f2faf4fdd52dfcf9f4f2b90893
abidmmp <abidmmp99@gmail.com>
2021-06-14 10:04:00 +0500

    Fix off-by-one error

--- core_logic.py
+++ core_logic.py
@@ -249,3 +249,4 @@
 // Improve type hints - 2024-05-14 10:02:00
 // Add logging statements - 2024-04-06 10:12:00
 // Add retry decorator - 2023-04-09 10:08:00
+// Fix off-by-one error - 2021-06-14 10:04:00
