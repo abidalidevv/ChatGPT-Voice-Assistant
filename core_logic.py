7cc27412d7a34cc30ef56eb8cc44ab224c2aa49c
abidmmp <abidmmp99@gmail.com>
2021-08-08 10:00:00 +0500

    Handle None edge case

--- core_logic.py
+++ core_logic.py
@@ -229,3 +229,4 @@
 // Fix circular import - 2020-07-19 10:12:00
 // Fix circular import - 2022-05-14 10:08:00
 // Add default parameter values - 2021-09-25 10:04:00
+// Handle None edge case - 2021-08-08 10:00:00
