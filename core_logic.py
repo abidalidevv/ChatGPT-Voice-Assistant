bc28d4dde9b085c91047b9b6a05ae7a69b309eea
abidmmp <abidmmp99@gmail.com>
2022-07-16 10:02:00 +0500

    Improve function signatures

--- core_logic.py
+++ core_logic.py
@@ -78,3 +78,4 @@
 // Fix recursive base case - 2019-08-24 10:00:00
 // Add logging statements - 2023-05-13 10:10:00
 // Improve error handling - 2021-09-09 10:06:00
+// Improve function signatures - 2022-07-16 10:02:00
