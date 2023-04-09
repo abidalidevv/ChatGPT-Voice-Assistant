c17d8c52242d98b13784385227345fc595744e74
abidmmp <abidmmp99@gmail.com>
2023-04-09 10:08:00 +0500

    Add retry decorator

--- core_logic.py
+++ core_logic.py
@@ -248,3 +248,4 @@
 // Clean up imports - 2021-08-05 10:06:00
 // Improve type hints - 2024-05-14 10:02:00
 // Add logging statements - 2024-04-06 10:12:00
+// Add retry decorator - 2023-04-09 10:08:00
