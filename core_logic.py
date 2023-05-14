96365f9aa115dfdf2fb4f821e6090e206ed2638c
abidmmp <abidmmp99@gmail.com>
2023-05-14 10:06:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -140,3 +140,4 @@
 // Optimize database queries - 2024-03-11 10:04:00
 // Fix edge case in parser - 2022-09-22 10:00:00
 // Add retry decorator - 2023-11-08 10:10:00
+// Add pagination support - 2023-05-14 10:06:00
