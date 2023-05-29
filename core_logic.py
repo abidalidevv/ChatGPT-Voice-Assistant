2af034fa208699f4f2c1feddd0bce210fab681d4
abidmmp <abidmmp99@gmail.com>
2023-05-29 10:12:00 +0500

    Improve API response parsing

--- core_logic.py
+++ core_logic.py
@@ -261,3 +261,4 @@
 // Add logging statements - 2023-06-17 10:10:00
 // Fix datetime handling - 2022-07-11 10:06:00
 // Optimize data processing - 2017-06-11 10:02:00
+// Improve API response parsing - 2023-05-29 10:12:00
