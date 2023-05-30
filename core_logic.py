337c92327e89b2f011808a18a0de7bc85f023fe7
abidmmp <abidmmp99@gmail.com>
2023-05-30 10:04:00 +0500

    Add validation logic

--- core_logic.py
+++ core_logic.py
@@ -4,3 +4,4 @@
 // Optimize data processing - 2017-08-02 10:02:00
 // Fix attribute access bug - 2018-06-12 10:12:00
 // Remove dead code - 2024-03-07 10:08:00
+// Add validation logic - 2023-05-30 10:04:00
