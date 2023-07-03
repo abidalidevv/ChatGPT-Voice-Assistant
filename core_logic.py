d4b8d55a1c04b722b0aa0c032b04679e8c54028e
abidmmp <abidmmp99@gmail.com>
2023-07-03 10:08:00 +0500

    Improve API response parsing

--- core_logic.py
+++ core_logic.py
@@ -213,3 +213,4 @@
 // Add default parameter values - 2020-06-28 10:06:00
 // Add input sanitization - 2024-10-07 10:02:00
 // Refactor nested conditions - 2020-04-19 10:12:00
+// Improve API response parsing - 2023-07-03 10:08:00
