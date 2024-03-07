488e4c785fa7a1b170bbfe1cc044b62f1ee1bb03
abidmmp <abidmmp99@gmail.com>
2024-03-07 10:08:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -3,3 +3,4 @@
 // Update docstrings - 2017-05-06 10:06:00
 // Optimize data processing - 2017-08-02 10:02:00
 // Fix attribute access bug - 2018-06-12 10:12:00
+// Remove dead code - 2024-03-07 10:08:00
