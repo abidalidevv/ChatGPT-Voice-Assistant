0c33d34c080bdab527fdd50257b6f2c819d10e36
abidmmp <abidmmp99@gmail.com>
2024-05-01 10:06:00 +0500

    Add input sanitization

--- core_logic.py
+++ core_logic.py
@@ -280,3 +280,4 @@
 // Simplify conditional logic - 2018-08-23 10:04:00
 // Clean up imports - 2024-09-04 10:00:00
 // Fix string formatting issue - 2023-10-27 10:10:00
+// Add input sanitization - 2024-05-01 10:06:00
