fadc652c81581d26799e4f21ea7bc44bb2b9837c
abidmmp <abidmmp99@gmail.com>
2024-08-18 10:06:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -119,3 +119,4 @@
 // Optimize data processing - 2021-02-07 10:04:00
 // Simplify conditional logic - 2019-06-23 10:00:00
 // Refactor utility functions - 2020-08-12 10:10:00
+// Remove dead code - 2024-08-18 10:06:00
